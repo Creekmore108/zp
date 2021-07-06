cf19e53bf35d16ed67d1dec682cbaae5702a7695
Steve Creekmore <Creekmore108@gmail.com>
2021-07-06 12:22:29 -0600

    added livewire

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        @livewireStyles
         <title>Laravel</title>
 
         <!-- Fonts -->
@@ -159,5 +159,6 @@
                 </div>
             </div>
         </div>
+        @livewireScripts
     </body>
 </html>
